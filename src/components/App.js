380e0677a93e8f8e494e8d7ada79b316aaa3b307
Sohan Chowdhury <sifat3d@gmail.com>
2017-11-17 21:10:35 +0600

    Moved to Public folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './Home/HomePage';
+
+
+//Import Public Pages
+import HomePage from './Public/HomePage';
 import FuelSavingsPage from '../containers/FuelSavingsPage';
-import AboutPage from './AboutPage';
-import NotFoundPage from './NotFoundPage';
+import AboutPage from './Public/AboutPage';
+import NotFoundPage from './Public/NotFoundPage';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -16,10 +19,13 @@ class App extends React.Component {
     return (
       <div>
         <Switch>
+
+          {/* PUBLIC ROUTES */}
           <Route exact path="/" component={HomePage} />
           <Route path="/fuel-savings" component={FuelSavingsPage} />
           <Route path="/about" component={AboutPage} />
           <Route component={NotFoundPage} />
+
         </Switch>
       </div>
     );
